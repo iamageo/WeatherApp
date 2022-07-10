52804bbc4403e2ebcf406d1c27bc44ade1433eaf
iamageo <geovani.af4@gmail.com>
2022-07-10 14:05:24 -0300

    update mappers

--- app/src/main/java/com/iamageo/weather/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/iamageo/weather/data/mappers/WeatherMappers.kt
@@ -1,11 +1,20 @@
 package com.iamageo.weather.data.mappers
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.iamageo.weather.data.remote.WeatherDataDto
 import com.iamageo.weather.domain.weather.WeatherData
+import com.iamageo.weather.domain.weather.WeatherInfo
 import com.iamageo.weather.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+private data class IndexedWeatherData(
+    val index: Int,
+    val data: WeatherData
+)
+
+@RequiresApi(Build.VERSION_CODES.O)
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
     return time.mapIndexed { index, time ->
         val temp = temperatures[index]
@@ -13,15 +22,20 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
         val windSpeed = windSpeeds[index]
         val pressure = pressures[index]
         val humidity = humidities[index]
-        WeatherData(
-            time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
-            temperatureCelcius = temp,
-            pressure = pressure,
-            windSpeed = windSpeed,
-            humidity = humidity,
-            weatherType = WeatherType.fromWMO(weatherCode)
+        IndexedWeatherData(
+            index = index,
+            data = WeatherData(
+                time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
+                temperatureCelcius = temp,
+                pressure = pressure,
+                windSpeed = windSpeed,
+                humidity = humidity,
+                weatherType = WeatherType.fromWMO(weatherCode)
+            )
         )
-    }.groupBy { weatherData ->
-        weatherData.time.dayOfMonth
+    }.groupBy {
+        it.index / 24
+    }.mapValues {
+        it.value.map { it.data }
     }
 }
