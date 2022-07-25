8b4c7bf08718d7d040a30b5fb0a57ba25c6c8eaa
iamageo <geovani.af4@gmail.com>
2022-07-24 22:41:11 -0300

    update ui

--- app/src/main/java/com/iamageo/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/iamageo/weather/presentation/MainActivity.kt
@@ -8,13 +8,18 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.iamageo.weather.presentation.theme.WeatherTheme
 import com.iamageo.weather.presentation.ui.WeatherViewModel
 import com.iamageo.weather.presentation.ui.components.WeatherCard
+import com.iamageo.weather.presentation.ui.components.WeatherForecast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,15 +40,33 @@ class MainActivity : ComponentActivity() {
             arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
-            ))
+            )
+        )
         setContent {
             WeatherTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.DarkGray)
-                ) {
-                    WeatherCard(state = viewModel.state, backgroundColor = Color.LightGray)
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.DarkGray)
+                    ) {
+                        WeatherCard(state = viewModel.state, backgroundColor = Color.LightGray)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if (viewModel.state.isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/iamageo/weather/presentation/ui/components/HourlyWeatherDisplay.kt
@@ -0,0 +1,52 @@
+package com.iamageo.weather.presentation.ui.components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.iamageo.weather.domain.weather.WeatherData
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun HourlyWeatherDisplay(
+    weatherData: WeatherData,
+    modifier: Modifier = Modifier,
+    textColor: Color = Color.White
+) {
+    val formattedTime = remember(weatherData) {
+        weatherData.time.format(
+            DateTimeFormatter.ofPattern("HH:mm")
+        )
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = formattedTime,
+            color = Color.LightGray
+        )
+        Image(
+            painter = painterResource(id = weatherData.weatherType.iconRes),
+            contentDescription = null,
+            modifier = Modifier.width(40.dp)
+        )
+        Text(text = "${weatherData.temperatureCelcius}ºC",
+            color = textColor,
+            fontWeight = FontWeight.Bold
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
