9ee078cf40a8d399e282a4f0ef404df929422406
iamageo <geovani.af4@gmail.com>
2022-08-08 18:26:18 -0300

    adapting weatherForecast to show daily info and other days

--- app/src/main/java/com/iamageo/weather/presentation/ui/components/WeatherForecast.kt
+++ app/src/main/java/com/iamageo/weather/presentation/ui/components/WeatherForecast.kt
@@ -2,22 +2,36 @@ package com.iamageo.weather.presentation.ui.components
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.iamageo.weather.domain.weather.WeatherType
 import com.iamageo.weather.presentation.ui.WeatherState
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun WeatherForecast(
     state: WeatherState,
-    modifier: Modifier = Modifier,
-    day: Int?= 0
+    day: Int
 ) {
+    val weatherDataList = state.weatherInfo?.weatherDataPerDay
+
     val listOfColors by lazy {
         listOf(
             Color(0xfff44336),
@@ -40,19 +54,107 @@ fun WeatherForecast(
             Color(0xff9e9e9e),
         )
     }
-    state.weatherInfo?.weatherDataPerDay?.get(day)?.let { data ->
-        Column(
-            modifier = modifier
-                .padding(horizontal = 8.dp)
-        ) {
 
-            HourlyWeatherDisplay(
-                weatherData = data,
-                modifier = Modifier
-                    .height(100.dp)
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                backgroundColor = listOfColors[day!!]
+    val dayName = when (day) {
+        0 -> "Today"
+        1 -> "Tomorrow"
+        else -> {
+            LocalDateTime
+                .now()
+                .plusDays(day.toLong())
+                .dayOfWeek
+                .toString()
+                .lowercase()
+                .replaceFirstChar {
+                    it.uppercase()
+                }
+        }
+    }
+
+    if (day >= 1) {
+        val weatherTypeList = ArrayList<Int>()
+        var maxTemperature = 0.0
+        var minTemperature = 60.0
+        weatherDataList?.get(day)
+            ?.map {
+                weatherTypeList.add(it.weatherType.weatherCode)
+                maxTemperature = if (maxTemperature > it.temperatureCelcius){
+                    maxTemperature
+                } else {
+                    it.temperatureCelcius
+                }
+                minTemperature = if (minTemperature < it.temperatureCelcius){
+                    minTemperature
+                } else {
+                    it.temperatureCelcius
+                }
+            }
+            ?.toList()
+        val weatherAverageCode = weatherTypeList
+            .groupingBy { it }
+            .eachCount()
+            .maxByOrNull { it.value }
+            ?.key
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Divider(
+                color = Color.Gray
+                    .copy(
+                        alpha = 0.5f
+                    ),
+                thickness = 0.2.dp
+            )
+            DailyWeatherDisplay(
+                dayName = dayName,
+                weatherType = WeatherType.fromWMO(weatherAverageCode ?: 0),
+                maxTemperature = maxTemperature,
+                minTemperature = minTemperature,
+                backgroundColor = listOfColors[(0..17).random()]
             )
         }
+    } else {
+        val localTime = LocalDateTime.now()
+        localTime.format(DateTimeFormatter.ISO_DATE_TIME)
+        weatherDataList?.get(day)
+            ?.filter { filterData ->
+                filterData.time.hour >= localTime.hour - 1
+                        && filterData.time.dayOfMonth == localTime.dayOfMonth
+            }
+            ?.toList()
+            ?.let { data ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = dayName,
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = Color.Black,
+                            fontWeight = FontWeight.SemiBold,
+                        ),
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                    Divider(
+                        color = Color.Gray
+                            .copy(
+                                alpha = 0.5f
+                            ),
+                        thickness = 0.2.dp
+                    )
+                    LazyRow(
+                        modifier = Modifier
+                            .padding(vertical = 16.dp),
+                        content = {
+                            items(data) { weatherData ->
+                                HourlyWeatherDisplay(weatherData = weatherData)
+                            }
+                        }
+                    )
+                }
+            }
     }
 }
