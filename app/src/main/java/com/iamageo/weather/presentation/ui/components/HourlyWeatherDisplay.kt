06f03f5ab49e47b78d4763ca52af63f31d889f14
iamageo <geovani.af4@gmail.com>
2022-08-08 18:28:45 -0300

    updating today info list

--- app/src/main/java/com/iamageo/weather/presentation/ui/components/HourlyWeatherDisplay.kt
+++ app/src/main/java/com/iamageo/weather/presentation/ui/components/HourlyWeatherDisplay.kt
@@ -15,51 +15,56 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.iamageo.weather.domain.weather.WeatherData
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HourlyWeatherDisplay(
-    weatherData: List<WeatherData>,
-    modifier: Modifier = Modifier,
-    textColor: Color = Color.White,
-    backgroundColor: Color = MaterialTheme.colors.secondary
+    weatherData: WeatherData
 ) {
-    weatherData.get(0).let { data ->
-        val formattedTime = remember(weatherData) {
-            data.time.format(
-                DateTimeFormatter.ofPattern("HH:mm")
+    val formattedTime = remember(weatherData) {
+        if (LocalDateTime.now().hour == weatherData.time.hour) {
+            "Now"
+        } else {
+            weatherData.time.format(
+                DateTimeFormatter.ofPattern("HH")
             )
         }
-        Card(
-            backgroundColor = backgroundColor,
-            shape = RoundedCornerShape(10.dp),
-        ) {
-            Column(
-                modifier = modifier,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = formattedTime,
-                    color = Color.White
-                )
-                Image(
-                    painter = painterResource(id = data.weatherType.iconRes),
-                    contentDescription = null,
-                    modifier = Modifier.width(40.dp)
-                )
-                Text(
-                    text = "${data.temperatureCelcius}ºC",
-                    color = textColor,
-                    fontWeight = FontWeight.Bold
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-        }
     }
-
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = formattedTime,
+            style = TextStyle(
+                fontSize = 18.sp,
+                color = Color.Gray,
+                fontWeight = FontWeight.SemiBold,
+            ),
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(
+            painter = painterResource(id = weatherData.weatherType.iconRes),
+            contentDescription = weatherData.weatherType.weatherDesc,
+            modifier = Modifier.size(34.dp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "${weatherData.temperatureCelcius.toInt()}°",
+            style = TextStyle(
+                fontSize = 22.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.SemiBold,
+            ),
+        )
+    }
 }
